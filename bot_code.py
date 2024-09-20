e27f676af02d1aef2578514c49da5ff1bdde4ab2
BluePyTheDeer_ <bluepythewolfdeer@proton.me>
2024-09-20 14:06:29 -0600

    Adding some code

--- bot_code.py
+++ bot_code.py
@@ -20,4 +20,9 @@ if os == "macOS":
 if os == "Linux":
     print("Very nice, but depending on the distro, it can have its ups and downs.")
 
-print("That's all I can do, for now.")
+genre = input("Ok, so, what genres of games do you enjoy? ")
+
+totalGenre = print(f"Oh, {genres} is/are very nice genre(s)!")
+
+
+print("That's all I can do, for now, if I ever actually get improved, which is somewhat unlikely considering a lot of things, but maybe, against all odds, this could get improved!")
